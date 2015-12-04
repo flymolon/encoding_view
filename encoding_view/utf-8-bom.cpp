e866442e47e0fb7890b8b863a69eee51f48ba453
flymolon <flymolon@126.com>
2015-12-04 13:00:56 +0800

    adding utf-8 with bom

---
+++ encoding_view/utf-8-bom.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+
+
+static void test()
+{
+	const char *p_cn = "ÄãºÃ";
+	const char *p_jap = "¤³¤ó¤Ë¤Á¤Ï";
+	const char *p_ti = "??????";
+	const char *p_russia = "§©§Õ§â§Ñ§Ó§ã§ä§Ó§å§Û§ä§Ö";
+}
