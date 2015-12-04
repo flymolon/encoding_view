fe7c3bc21f6480b0f13aaa29ba024ce4877bbd03
flymolon <flymolon@126.com>
2015-12-04 12:49:13 +0800

    adding test files

---
+++ encoding_view/utf-8.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+
+
+static void test()
+{
+	const char *p_cn = "你好";
+	const char *p_jap = "こんにちは";
+	const char *p_ti = "สวัสดี";
+	const char *p_russia = "Здравствуйте";
+}
